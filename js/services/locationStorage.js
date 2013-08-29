b48f0ad1777790b931dbec1ae5c0e448fa8f1fc5
Patrick Ng <ckipel@gmail.com>
2013-08-29 11:45:57 -0400

    rough demo of multiple storage service solution

--- js/services/locationStorage.js
+++ js/services/locationStorage.js
@@ -3,13 +3,30 @@
 app.factory('locationStorageService', function() {
 	var STORAGE_ID = 'locations-angularjs';
 
-	return {
-		get: function() {
-			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-		},
+	// local storage
+	if (!window.localStorage) {
 
-		set: function(locations) {
-			localStorage.setItem(STORAGE_ID, JSON.stringify(locations));
-		}
-	};
+
+		return {
+			get: function() {
+				return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+			},
+
+			set: function(locations) {
+				localStorage.setItem(STORAGE_ID, JSON.stringify(locations));
+			}
+		};
+
+	}
+
+	// ajax storage
+
+	// return {
+	// 	get: function() {
+	// 		alert('i was retrieved from server');
+	// 	},
+	// 	set: function(value) {
+	// 		alert('im storing my shiz on the server');
+	// 	}
+	// }
 });
