c24554530e6a282b7403e0f0bb392999b05b7edd
Patrick Ng <ckipel@gmail.com>
2013-09-06 16:59:02 -0400

    updating grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -46,8 +46,10 @@ module.exports = function(grunt) {
       }
     },
     watch: {
-      files: ["**/*.html", "**/*.js",  "**/js/**"],
-      tasks: 'reload'
+      files: ["**/*.html", "**/*.js",  "**/js/**", "css/*.css"],
+      options: {
+        livereload: true
+      }
     }
   });
 
@@ -60,13 +62,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-requirejs');
 
   // Default task.
-  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'reload']);
-  grunt.registerTask("reload", "reload Chrome on OS X",
-    function() {
-      require("child_process").exec("osascript " +
-        "-e 'tell application \"Google Chrome\" " +
-          "to tell the active tab of its first window' " +
-        "-e 'reload' " +
-        "-e 'end tell'");
-    });
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin']);
 };
