d6d02302aeaf362bd39478946f9313b7b2dd3103
Patrick Ng <ckipel@gmail.com>
2013-09-06 16:58:34 -0400

    Revert "rough demo of multiple storage service solution"
    This reverts commit b48f0ad1777790b931dbec1ae5c0e448fa8f1fc5.

--- js/services/locationStorage.js
+++ js/services/locationStorage.js
@@ -3,30 +3,13 @@
 app.factory('locationStorageService', function() {
 	var STORAGE_ID = 'locations-angularjs';
 
-	// local storage
-	if (!window.localStorage) {
+	return {
+		get: function() {
+			return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+		},
 
-
-		return {
-			get: function() {
-				return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
-			},
-
-			set: function(locations) {
-				localStorage.setItem(STORAGE_ID, JSON.stringify(locations));
-			}
-		};
-
-	}
-
-	// ajax storage
-
-	// return {
-	// 	get: function() {
-	// 		alert('i was retrieved from server');
-	// 	},
-	// 	set: function(value) {
-	// 		alert('im storing my shiz on the server');
-	// 	}
-	// }
+		set: function(locations) {
+			localStorage.setItem(STORAGE_ID, JSON.stringify(locations));
+		}
+	};
 });
